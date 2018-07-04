fde33d773bfa7f1e919d1c4c69deb5fd33c4030f
quangogage <quangogage@users.noreply.github.com>
2018-07-04 00:44:53 -0700

    Started to set up the  class

--- js/handlers/BulletHandler.js
+++ js/handlers/BulletHandler.js
@@ -2,10 +2,20 @@ export default class BulletHandler {
   constructor(scene) {
     // Store values
     this.scene = scene;
+
+    // The bullet instances
+    this.bullets = [];
   }
 
   /* ** Public Functions ** */
-  preload() {}
+  preload() {
+    // Bullet types
+    this.bulletData = [
+      {
+        name: 'basic'
+      }
+    ];
+  }
   create() {}
   update() {}
 }
